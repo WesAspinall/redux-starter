577e3594b8ea4e0d5f389f0a30c3c75473fad838
Wesley Aspinall <wra427@gmail.com>
2017-04-13 01:28:56 -0400

    App layout + Component Setup

---
+++ client/components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+
+
+class Main extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1>
+          <Link to='/'>Westagram</Link>
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default Main;
