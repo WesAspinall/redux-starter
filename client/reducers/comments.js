40cc5ab558733bbe57da65f444453c572429b04d
Wesley Aspinall <wra427@gmail.com>
2017-04-16 16:02:36 -0400

    passed actionCreators down to Main component via redux connect()

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,10 +1,14 @@
+// a reducer takes in two things:
+
 // 1. the action (info about what happened)
 // 2. copy of current state
 
 
+// * also, every time an action is dispatched, every reducer will run. 
 
 function comments(state = [], action) {
   console.log(state, action);
   return state;
 }
+
 export default comments;
